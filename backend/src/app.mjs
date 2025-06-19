379e2191213ae1f84f620099855c7a55aa04d429
JoooooMei <c.johan.meisel@gmail.com>
2025-06-19 14:47:47 +0200

    Add security to app

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -1,10 +1,33 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
+import { xss } from 'express-xss-sanitizer';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './config/config.env' });
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'You are killing me, get lost!!!',
+});
+
 const app = express();
 
 app.use(express.json());
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use('/api/', limiter);
+
+app.use(xss());
+
+app.use(hpp());
+
+app.use(express.json({ limit: '10kb' }));
 
 export { app };
