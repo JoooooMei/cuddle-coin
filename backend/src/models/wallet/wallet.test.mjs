43ffd8bff5739056b84f86b79f617156ac91b352
JoooooMei <c.johan.meisel@gmail.com>
2025-06-26 14:05:44 +0200

    Wallet test

--- backend/src/models/wallet/wallet.test.mjs
+++ backend/src/models/wallet/wallet.test.mjs
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, it } from 'vitest';
-
 import Wallet from './Wallet.mjs';
+import { verifySignature } from '../../utilities/keyManager.mjs';
 
 describe('Wallet', () => {
   let wallet;
@@ -10,10 +10,35 @@ describe('Wallet', () => {
   });
 
   it('should have property: publicKey', () => {
+    console.log('Public key:', wallet.publicKey);
     expect(wallet).toHaveProperty('publicKey');
   });
 
   it('should have property: balance', () => {
     expect(wallet).toHaveProperty('balance');
+  });
+
+  describe('Signing data', () => {
+    const data = 'Dummy data';
+
+    it('should verify a valid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data: data,
+          signature: wallet.sign(data),
+        })
+      ).toBeTruthy();
+    });
+
+    it('should not verify an invalid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data: data,
+          signature: new Wallet().sign(data),
+        })
+      ).toBeFalsy();
+    });
   });
 });
