104d29bb813f5282f6ad1aa7c13cd4bfd7eb9b45
JoooooMei <c.johan.meisel@gmail.com>
2025-07-20 17:46:47 +0200

    adds roles to auth controller

--- backend/src/controllers/auth-controller.mjs
+++ backend/src/controllers/auth-controller.mjs
@@ -1,3 +1,4 @@
+import { promisify } from 'util';
 import jwt from 'jsonwebtoken';
 import { catchErrorAsync } from '../utilities/catchErrorAsync.mjs';
 import AppError from '../models/blockchain/appError.mjs';
@@ -20,13 +21,53 @@ export const loginUser = catchErrorAsync(async (req, res, next) => {
     return next(new AppError('Wrong email and/or password', 401));
   }
 
-  const token = createToken(user._id);
+  const token = createToken(user._id, user.role);
 
   res.status(200).json({ success: true, statusCode: 200, data: { token } });
 });
 
-const createToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+export const protect = catchErrorAsync(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.toLowerCase().startsWith('bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('You need to be logged in', 401));
+  }
+
+  const decoded = await verifyToken(token);
+
+  const user = await new UserRepository().getUser(decoded.id);
+
+  req.user = user;
+  next();
+});
+
+export const authorize = (...role) => {
+  return (req, res, next) => {
+    console.log('role: ', req.user.role);
+
+    console.log('Roles allowed', role);
+
+    if (!role.includes(req.user.role)) {
+      return next(new AppError('Peasents not allowed', 403));
+    }
+
+    next();
+  };
+};
+
+const createToken = (userId, userRole) => {
+  return jwt.sign({ id: userId, role: userRole }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES,
   });
+};
+
+const verifyToken = async (token) => {
+  return await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 };
--- backend/src/routes/user-routes.mjs
+++ backend/src/routes/user-routes.mjs
@@ -5,11 +5,12 @@ import {
   getUser,
   deleteUser,
 } from '../controllers/user-controller.mjs';
+import { protect, authorize } from '../controllers/auth-controller.mjs';
 
 const router = Router();
 
 router.route('/add').post(addUser);
-router.route('/').get(getAllUsers);
-router.route('/:id').get(getUser).delete(deleteUser);
+router.route('/').get(protect, authorize('admin'), getAllUsers);
+router.route('/:id').get(protect, getUser).delete(protect, deleteUser);
 
 export default router;
